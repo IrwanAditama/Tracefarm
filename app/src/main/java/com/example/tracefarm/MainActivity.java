7a285b7186932b649446587404e0d52846de0c73
IrwanAditama <irwanaditama1@gmail.com>
2019-05-29 15:12:20 +0700

    haha

---
+++ app/src/main/java/com/example/tracefarm/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tracefarm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_petani);
+        textView = findViewById(R.id.ptn);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tracefarm/login/Petani_login.java
@@ -0,0 +1,15 @@
+package com.example.tracefarm.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.tracefarm.R;
+
+public class Petani_login extends AppCompatActivity {
+@Override
+    protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login_petani);
+}
+}
+
