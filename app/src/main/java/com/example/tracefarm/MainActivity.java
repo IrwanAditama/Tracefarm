b4503d654fb7e7c5cce399c5e902507a43c69549
IrwanAditama <irwanaditama1@gmail.com>
2019-05-31 10:17:07 +0700

    update122

--- app/src/main/java/com/example/tracefarm/MainActivity.java
+++ app/src/main/java/com/example/tracefarm/MainActivity.java
@@ -1,23 +1,34 @@
 package com.example.tracefarm;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+   implements NavigationView.OnNavigationItemSelectedListener
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+    {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @Override
+        protected void onCreate (Bundle Bundle savedInstanceState;
+        savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_petani);
-        textView = findViewById(R.id.ptn);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
-
-
+    }
 }
